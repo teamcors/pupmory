6dc3af0a53c26326383db1be06960ee5979eb931
Queue-ri <qriositylog@gmail.com>
2023-06-03 19:23:23 +0900

    chore: Add Spring Boot template

---
+++ backend/src/main/java/com/hamahama/pupmory/PupmoryApplication.java
@@ -0,0 +1,13 @@
+package com.hamahama.pupmory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupmoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PupmoryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hamahama/pupmory/PupmoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamahama.pupmory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PupmoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
