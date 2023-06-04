f3af0094eca468c34dfd2bd0681f3d0288d8304e
Queue-ri <qriositylog@gmail.com>
2023-06-04 22:20:17 +0900

    feat: Add ServiceUser entity (#2)

---
+++ backend/src/main/java/com/hamahama/pupmory/domain/user/ServiceUser.java
@@ -0,0 +1,44 @@
+package com.hamahama.pupmory.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author Queue-ri
+ * @since 2023/06/04
+ */
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceUser {
+    // User는 PostgreSQL에서 reserved keyword임에 유의
+    @Id
+    private String userUid;
+
+    private String profileImage;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private String puppyName;
+
+    @Column(nullable = false)
+    private Long conversationStatus;
+
+    private String characterData;
+
+    @Column(nullable = false)
+    private Long memoryCount;
+}
---
+++ backend/src/main/java/com/hamahama/pupmory/domain/user/ServiceUserRepository.java
@@ -0,0 +1,11 @@
+package com.hamahama.pupmory.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Queue-ri
+ * @since 2023/06/04
+ */
+
+public interface ServiceUserRepository extends JpaRepository<ServiceUser, Long> {
+}
