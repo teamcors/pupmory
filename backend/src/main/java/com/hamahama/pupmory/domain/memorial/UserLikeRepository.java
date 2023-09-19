30f88f28790a01d2881474909be7893b223d605e
Queue-ri <qriositylog@gmail.com>
2023-09-20 07:12:36 +0900

    feat: Add user like (#24)

---
+++ backend/src/main/java/com/hamahama/pupmory/domain/memorial/UserLikeRepository.java
@@ -0,0 +1,9 @@
+package com.hamahama.pupmory.domain.memorial;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
+    Optional<UserLike> findByUserUidAndPostId(String userUid, Long postId);
+}
