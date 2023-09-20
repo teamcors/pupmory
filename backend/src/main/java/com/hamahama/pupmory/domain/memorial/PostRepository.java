efecbb579e83d0cf914f675e38f2fce4741d45fd
Queue-ri <qriositylog@gmail.com>
2023-09-21 03:12:04 +0900

    fix: Exclude private posts from feed request (#26)

--- backend/src/main/java/com/hamahama/pupmory/domain/memorial/PostRepository.java
+++ backend/src/main/java/com/hamahama/pupmory/domain/memorial/PostRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByUserUid(String uuid);
 
-    @Query("SELECT p FROM Post p WHERE p.userUid <> :userUid ORDER BY p.createdAt DESC")
+    @Query("SELECT p FROM Post p WHERE p.userUid <> :userUid AND p.isPrivate = false ORDER BY p.createdAt DESC")
     List<Post> findLatestFeed(@Param("userUid") String uuid);
 
     // 피드 필터 조회
@@ -17,7 +17,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "FROM post\n" +
             "    INNER JOIN service_user\n" +
             "    ON post.user_uid = service_user.user_uid\n" +
-            "WHERE post.user_uid != :userUid AND puppy_type = :puppyType\n" +
+            "WHERE post.user_uid != :userUid AND puppy_type = :puppyType AND is_private = false\n" +
             "ORDER BY created_at DESC;", nativeQuery = true)
     List<Post> findFilteredFeedByType(@Param("userUid") String uuid, @Param("puppyType") String type);
 
@@ -25,7 +25,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "FROM post\n" +
             "    INNER JOIN service_user\n" +
             "    ON post.user_uid = service_user.user_uid\n" +
-            "WHERE post.user_uid != :userUid AND puppy_age = :puppyAge\n" +
+            "WHERE post.user_uid != :userUid AND puppy_age = :puppyAge AND is_private = false\n" +
             "ORDER BY created_at DESC;", nativeQuery = true)
     List<Post> findFilteredFeedByAge(@Param("userUid") String uuid, @Param("puppyAge") Integer age);
 
@@ -33,7 +33,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "FROM post\n" +
             "    INNER JOIN service_user\n" +
             "    ON post.user_uid = service_user.user_uid\n" +
-            "WHERE post.user_uid != :userUid AND puppy_type = :puppyType AND puppy_age = :puppyAge\n" +
+            "WHERE post.user_uid != :userUid AND puppy_type = :puppyType AND puppy_age = :puppyAge AND is_private = false\n" +
             "ORDER BY created_at DESC;", nativeQuery = true)
     List<Post> findFilteredFeedByBoth(@Param("userUid") String uuid, @Param("puppyType") String type, @Param("puppyAge") Integer age);
 }
