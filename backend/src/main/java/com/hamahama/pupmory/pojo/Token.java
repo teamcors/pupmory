6d5479c4f59a975e47af190fd4d20e8bd9f2ab75
Queue-ri <qriositylog@gmail.com>
2023-10-21 01:31:23 +0900

    feat: Add sign in API (#37)

---
+++ backend/src/main/java/com/hamahama/pupmory/pojo/Token.java
@@ -0,0 +1,12 @@
+package com.hamahama.pupmory.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Token {
+    private String accessToken;
+}
