e4abf21ef238c05964e34c82d4bfc1110e6e3d2d
Queue-ri <qriositylog@gmail.com>
2023-11-15 02:42:18 +0900

    feat: Add conversation GPT emotion API (#58)

--- backend/src/main/java/com/hamahama/pupmory/controller/ConversationController.java
+++ backend/src/main/java/com/hamahama/pupmory/controller/ConversationController.java
@@ -1,8 +1,10 @@
 package com.hamahama.pupmory.controller;
 
 import com.hamahama.pupmory.dto.*;
+import com.hamahama.pupmory.dto.conversation.EmotionRequestDto;
 import com.hamahama.pupmory.dto.user.UserInfoUpdateDto;
 import com.hamahama.pupmory.service.ConversationService;
+import com.hamahama.pupmory.service.GptService;
 import com.hamahama.pupmory.util.auth.JwtKit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,19 +23,23 @@ import java.util.List;
 @RequestMapping("conversation")
 public class ConversationController {
     private final ConversationService conversationService;
+    private final GptService gptService;
     private final JwtKit jwtKit;
 
+    // deprecated (prototype only)
     @PostMapping("/intro/info")
     public void saveUserInfo(@RequestHeader("Authorization") String token, @RequestBody UserInfoUpdateDto dto) {
         String uid = jwtKit.validate(token);
         conversationService.saveUserInfo(uid, dto);
     }
 
+    // deprecated (prototype only)
     @PostMapping("/set")
     public ResponseEntity<List<SetResponseDto>> getAvailableSet(@RequestBody SetRequestDto setRequestDto) {
         return new ResponseEntity<List<SetResponseDto>>(conversationService.getAvailableSet(setRequestDto.getUuid()), HttpStatus.OK);
     }
 
+    // deprecated (prototype only)
     @PostMapping("/answer")
     public ResponseEntity<AnswerResponseDto> getAnswer(@RequestBody AnswerRequestDto answerRequestDto) {
         return conversationService.getAnswer(
@@ -45,6 +51,7 @@ public class ConversationController {
         );
     }
 
+    // deprecated (prototype only)
     @PostMapping("/line")
     public ResponseEntity<List<LineResponseDto>> getAnswer(@RequestBody LineRequestDto lineRequestDto) {
         Long selected = -1L; // default
@@ -58,5 +65,12 @@ public class ConversationController {
                 lineRequestDto.getLineId(),
                 selected
         );
+    }
+
+    // stage 1 = 초기
+    // 판단해야 하는 감정 목록은 GptEmotionData 엔티티 참고
+    @PostMapping("/emotion/stage1/{setId}")
+    public ResponseEntity<?> getEmotion(@PathVariable Long setId, @RequestBody EmotionRequestDto dto) {
+        return gptService.getEmotion(1L, setId, dto.getUserAnswer());
     }
 }
