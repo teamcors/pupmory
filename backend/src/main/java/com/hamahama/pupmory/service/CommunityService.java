e50439da60245fa321ad157b64d315a7050f5600
Queue-ri <qriositylog@gmail.com>
2023-11-20 19:24:05 +0900

    fix: Set default `prev_list` value (#71)

--- backend/src/main/java/com/hamahama/pupmory/service/CommunityService.java
+++ backend/src/main/java/com/hamahama/pupmory/service/CommunityService.java
@@ -205,7 +205,10 @@ public class CommunityService {
         // argument이므로 double quote가 필요하며 json double quote를 사전에 처리해야 함에 유의
         // platform에 따라 되기도 안되기도 해서 도커 환경 권장 (windows, ubuntu)
         String arg1 = "\"" + sentence + "\"";
-        String arg2 = wCloudRepo.findById(uid).get().getWords();
+        Optional<WordCloud> optWc = wCloudRepo.findById(uid);
+        String arg2;
+        if (optWc.isPresent()) arg2 = optWc.get().getWords();
+        else arg2 = "[]";
 
         ProcessBuilder pb = new ProcessBuilder("python3", "wcloud.py", arg1, arg2);
         Process p = pb.start();
