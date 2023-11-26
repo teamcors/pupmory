23f99036e2ccbeab4a6b8251410e66357d37f61a
Queue-ri <qriositylog@gmail.com>
2023-11-26 14:41:48 +0900

    refactor: Add authentication interceptor to MemorialController (#1)
    Co-authored-by: hyojeongchoi <hyojeong113@naver.com>
    Co-authored-by: nusuy <nsuy.ch@gmail.com>
    Co-authored-by: chaeeun-Han <chaeeun.h911@gmail.com>

--- backend/src/main/java/com/hamahama/pupmory/controller/MemorialController.java
+++ backend/src/main/java/com/hamahama/pupmory/controller/MemorialController.java
@@ -1,5 +1,6 @@
 package com.hamahama.pupmory.controller;
 
+import com.hamahama.pupmory.conf.NoAuth;
 import com.hamahama.pupmory.domain.memorial.Comment;
 import com.hamahama.pupmory.dto.memorial.*;
 import com.hamahama.pupmory.service.MemorialService;
@@ -23,44 +24,39 @@ import java.util.List;
 @RequestMapping("memorial")
 public class MemorialController {
     private final MemorialService memorialService;
-    private final JwtKit jwtKit;
 
     @RequestMapping(method=RequestMethod.POST, consumes = {"multipart/form-data"})
-    public void savePost(@RequestHeader(value="Authorization") String token,
+    public void savePost(@RequestAttribute("uid") String uid,
                          @RequestPart("json") PostRequestDto dto,
                          @RequestPart("image") @Nullable List<MultipartFile> mfiles
     ) throws IOException {
-        String uid = jwtKit.validate(token);
         memorialService.savePost(uid, dto, mfiles);
     }
 
     @RequestMapping(method=RequestMethod.GET)
-    public PostDetailResponseDto getPost(@RequestHeader(value="Authorization") String token, @RequestParam Long postId) {
-        String uid = jwtKit.validate(token);
+    public PostDetailResponseDto getPost(@RequestAttribute("uid") String uid, @RequestParam Long postId) {
+        // uid를 필요로 하진 않지만 validate해야 함
         return memorialService.getPost(postId);
     }
 
     @RequestMapping(method=RequestMethod.DELETE)
-    public ResponseEntity<?> deletePost(@RequestHeader(value="Authorization") String token, @RequestParam Long postId) {
-        String uid = jwtKit.validate(token);
+    public ResponseEntity<?> deletePost(@RequestAttribute("uid") String uid, @RequestParam Long postId) {
         return memorialService.deletePost(uid, postId);
     }
 
     @GetMapping("/all")
-    public PostAllResponseDto getAllPost(@RequestHeader(value="Authorization") String token) {
-        String uid = jwtKit.validate(token);
+    public PostAllResponseDto getAllPost(@RequestAttribute("uid") String uid) {
         return memorialService.getAllPost(uid);
     }
 
     @GetMapping("guest/all")
-    public PostAllResponseDto getOthersAllPost(@RequestHeader(value="Authorization") String token, @RequestParam String targetUid) {
-        String myUid = jwtKit.validate(token);
+    public PostAllResponseDto getOthersAllPost(@RequestAttribute("uid") String uid, @RequestParam String targetUid) {
+        // uid를 필요로 하진 않지만 validate해야 함
         return memorialService.getOthersAllPost(targetUid);
     }
 
     @PostMapping("/feed")
-    public List<FeedPostResponseDto> getFeed(@RequestParam Boolean filter, @RequestHeader(value="Authorization") String token, @RequestBody(required=false) FeedPostFilterRequestDto dto) {
-        String uid = jwtKit.validate(token);
+    public List<FeedPostResponseDto> getFeed(@RequestParam Boolean filter, @RequestAttribute("uid") String uid, @RequestBody(required=false) FeedPostFilterRequestDto dto) {
         if (filter)
             return memorialService.getFeedByFilter(uid, dto);
         else
@@ -68,31 +64,28 @@ public class MemorialController {
     }
 
     @GetMapping("/like")
-    public boolean getLike(@RequestHeader(value="Authorization") String token, @RequestParam Long postId) {
-        String uid = jwtKit.validate(token);
+    public boolean getLike(@RequestAttribute("uid") String uid, @RequestParam Long postId) {
         return memorialService.getLike(uid, postId);
     }
 
     @PostMapping("/like")
-    public void processLike(@RequestHeader(value="Authorization") String token, @RequestParam Long postId) {
-        String uid = jwtKit.validate(token);
+    public void processLike(@RequestAttribute("uid") String uid, @RequestParam Long postId) {
         memorialService.processLike(uid, postId);
     }
 
     @GetMapping("/comment")
+    @NoAuth
     public List<CommentResponseDto> getComment(@RequestParam Long postId) {
         return memorialService.getComment(postId);
     }
 
     @PostMapping("/comment")
-    public void saveComment(@RequestHeader(value="Authorization") String token, @RequestParam Long postId, @RequestBody CommentRequestDto dto) {
-        String uid = jwtKit.validate(token);
+    public void saveComment(@RequestAttribute("uid") String uid, @RequestParam Long postId, @RequestBody CommentRequestDto dto) {
         memorialService.saveComment(uid, postId, dto);
     }
 
     @DeleteMapping("/comment")
-    public ResponseEntity<?> deleteComment(@RequestHeader(value="Authorization") String token, @RequestParam Long commentId) {
-        String uid = jwtKit.validate(token);
+    public ResponseEntity<?> deleteComment(@RequestAttribute("uid") String uid, @RequestParam Long commentId) {
         return memorialService.deleteComment(uid, commentId);
     }
 }
