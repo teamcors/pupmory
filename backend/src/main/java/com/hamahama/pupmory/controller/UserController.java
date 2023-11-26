34d06ec5e9f279cfba2e608827dd9b9fde4d37fb
Queue-ri <qriositylog@gmail.com>
2023-11-26 14:43:16 +0900

    refactor: Add authentication interceptor to UserController (#1)
    Co-authored-by: hyojeongchoi <hyojeong113@naver.com>
    Co-authored-by: nusuy <nsuy.ch@gmail.com>
    Co-authored-by: chaeeun-Han <chaeeun.h911@gmail.com>

--- backend/src/main/java/com/hamahama/pupmory/controller/UserController.java
+++ backend/src/main/java/com/hamahama/pupmory/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.hamahama.pupmory.controller;
 
+import com.hamahama.pupmory.conf.NoAuth;
 import com.hamahama.pupmory.domain.user.ServiceUser;
 import com.hamahama.pupmory.dto.*;
 import com.hamahama.pupmory.dto.memorial.PostRequestDto;
@@ -29,23 +30,21 @@ import java.util.List;
 public class UserController {
     private final ConversationService conversationService;
     private final UserService userService;
-    private final JwtKit jwtKit;
 
     // deprecated (for prototype only)
     @GetMapping("/home")
+    @NoAuth
     public ResponseEntity<List<SetResponseDto>> getAvailableSet(@RequestBody SetRequestDto setRequestDto) {
         return new ResponseEntity<List<SetResponseDto>>(conversationService.getAvailableSet(setRequestDto.getUuid()), HttpStatus.OK);
     }
 
     @GetMapping("/info")
-    public UserInfoResponseDto getUserInfo(@RequestHeader("Authorization") String token){
-        String uid = jwtKit.validate(token);
+    public UserInfoResponseDto getUserInfo(@RequestAttribute("uid") String uid){
         return userService.getUserInfo(uid);
     }
 
     @PutMapping("/conversation-status")
-    public ResponseEntity<?> setUserConversationStatus(@RequestHeader("Authorization") String token, @RequestBody ConversationStatusUpdateDto dto){
-        String uid = jwtKit.validate(token);
+    public ResponseEntity<?> setUserConversationStatus(@RequestAttribute("uid") String uid, @RequestBody ConversationStatusUpdateDto dto){
         return userService.setUserConversationStatus(uid, dto);
     }
 
@@ -53,17 +52,15 @@ public class UserController {
     // json: 변경되지 않은 필드도 그대로 같이 보내줘야 함
     // image: 변경되지 않았을 경우 보내지 않아야 함 (null)
     @PostMapping("/info")
-    public ResponseEntity<?> updateUserInfo(@RequestHeader(value="Authorization") String token,
+    public ResponseEntity<?> updateUserInfo(@RequestAttribute("uid") String uid,
                                       @RequestPart("json") UserInfoUpdateDto dto,
                                       @RequestPart("image") @Nullable MultipartFile mfile) {
-        String uid = jwtKit.validate(token);
         return userService.updateUserInfo(uid, dto, mfile);
     }
 
     // 회원탈퇴
     @DeleteMapping("/account")
-    public ResponseEntity<?> deleteAccount(@RequestHeader("Authorization") String token) {
-        String uid = jwtKit.validate(token);
+    public ResponseEntity<?> deleteAccount(@RequestAttribute("uid") String uid) {
         return userService.deleteAccount(uid);
     }
 }
