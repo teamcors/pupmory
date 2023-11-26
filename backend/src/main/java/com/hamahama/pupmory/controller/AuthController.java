ce1f53461ae1cb01665bb1ad348ff35c6f58c698
Queue-ri <qriositylog@gmail.com>
2023-11-26 14:39:06 +0900

    refactor: Set NoAuth in AuthController (#1)
    Co-authored-by: hyojeongchoi <hyojeong113@naver.com>
    Co-authored-by: nusuy <nsuy.ch@gmail.com>
    Co-authored-by: chaeeun-Han <chaeeun.h911@gmail.com>

--- backend/src/main/java/com/hamahama/pupmory/controller/AuthController.java
+++ backend/src/main/java/com/hamahama/pupmory/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.hamahama.pupmory.controller;
 
+import com.hamahama.pupmory.conf.NoAuth;
 import com.hamahama.pupmory.dto.auth.SignUpRequestDto;
 import com.hamahama.pupmory.pojo.Token;
 import com.hamahama.pupmory.service.AuthService;
@@ -21,26 +22,31 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/token")
+    @NoAuth
     public Token getToken(@RequestHeader(value="X-Firebase-Token") String uid) {
         return authService.getToken(uid);
     }
 
     @GetMapping("/signin")
+    @NoAuth
     public ResponseEntity<?> signIn(@RequestHeader(value="X-Firebase-Token") String uid) {
         return authService.signIn(uid);
     }
 
     @PostMapping("/signup")
+    @NoAuth
     public ResponseEntity<?> signUp(@RequestBody SignUpRequestDto dto) {
         return authService.signUp(dto.getUserUid(), dto.getEmail());
     }
 
     @PostMapping("/email")
+    @NoAuth
     public ResponseEntity<?> sendValidationMail(@RequestParam String email) {
         return authService.sendValidationMail(email);
     }
 
     @GetMapping("/code")
+    @NoAuth
     public ResponseEntity<?> checkValidationCode(@RequestParam String code, @RequestParam String issuedBy) {
         return authService.checkValidationCode(code, issuedBy);
     }
