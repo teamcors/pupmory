58862141f6e93fe6d8eab6b48b9a077f1232e72e
Yusun Choi <nsuy.ch@gmail.com>
2024-03-14 16:45:32 +0900

    feat: Add RabbitMQ WcGen config (#4)
    Co-authored-by: chaeeun-Han <chaeeun.h911@gmail.com>
    Co-authored-by: Queue-ri <qriositylog@gmail.com>
    Co-authored-by: hyojeongchoi <hyojeong113@naver.com>

---
+++ backend/src/main/java/com/hamahama/pupmory/conf/RabbitWcGenConfig.java
@@ -0,0 +1,48 @@
+package com.hamahama.pupmory.conf;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitWcGenConfig {
+    private static final String queueName = "pupmory.wcgen";
+    private static final String topicExchangeName = "pupmory.wcgen.exchange";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("pupmory.wcgen");
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                  MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setReplyTimeout(300000);
+        rabbitTemplate.setReceiveTimeout(300000);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
